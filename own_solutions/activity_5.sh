eece2a969e124e399ea1683f33abfeb9b7787d62
Olivier Bonte <olivier.bonte@hotmail.com>
2025-10-24 10:07:14 +0200

    Add activity 5: running a python file with a container

---
+++ own_solutions/activity_5.sh
@@ -0,0 +1,8 @@
+# Pull the image only once 
+#docker pull jupyter/scipy-notebook:python-3.11.5
+
+# Problem with the joyvan folder 
+#docker run -v ../data:/home/joyvan jupyter/scipy-notebook:python-3.11.5 python codereppy_min_batch.py
+
+# Solution: just set to new working directory
+docker run -w /data -v ../data:/data jupyter/scipy-notebook:python-3.11.5 python codereppy_min_batch.py
